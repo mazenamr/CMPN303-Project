bcbb02f0069b89e1d2b835b5c8e64f717f447750
mazenamr <14795777+mazenamr@users.noreply.github.com>
2021-06-12 01:54:44 +0200

    Add Process

--- src/process.c
+++ src/process.c
@@ -1,18 +1,52 @@
 #include "headers.h"
 
-/* Modify this file as needed*/
-int remainingtime;
+void cont(int);
+void stop(int);
 
-int main(int agrc, char *argv[]) {
+int remainingTime;
+int startTime;
+int waitTime = 0;
+int stoppedTime;
+bool started = false;
+
+int main(int argc, char *argv[]) {
   initClk();
 
-  // TODO The process needs to get the remaining time from somewhere
-  // remainingtime = ??;
-  while (remainingtime > 0) {
-    // remainingtime = ??;
+  signal(SIGCONT, cont);
+  signal(SIGSTOP, stop);
+
+  if (argc < 2) {
+    printf("Too few arguments!\n");
+    exit(-1);
   }
+
+  remainingTime = atoi(argv[1]);
+
+  while (!started) {
+    usleep(DELAY_TIME);
+  }
+
+  startTime = getClk();
+
+  while (remainingTime > 0) {
+    int tick = getClk();
+    --remainingTime;
+    while (tick == getClk()) {
+      usleep(DELAY_TIME);
+    }
+  }
+
+  printf("Process %d died at %d with run time %d and wait time %d\n", getpid(),
+         getClk(), atoi(argv[1]), waitTime);
 
   destroyClk(false);
 
   return 0;
 }
+
+void cont(int signum) {
+  started = true;
+  waitTime += getClk() - stoppedTime;
+}
+
+void stop(int signum) { stoppedTime = getClk(); }
